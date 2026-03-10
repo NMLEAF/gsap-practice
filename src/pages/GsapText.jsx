7785feb7d8eff9bca3cedce35b856b9b7fb8ed49
NMLEAF <nathanmes124@gmail.com>
2026-03-10 10:59:31 +0300

    ✨ feat: add add gsap text animation

--- src/pages/GsapText.jsx
+++ src/pages/GsapText.jsx
@@ -1,9 +1,33 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapText = () => {
   // TODO: Implement gsap text animation
+  useGSAP(() => {
+    gsap.to("#text", {
+      x: 0,
+      opacity: 1,
+      ease: "power1.inOut",
+    });
+
+    gsap.fromTo(
+      ".para",
+      {
+        opacity: 0,
+        y: 20,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        delay: 0.5,
+        stagger: 0.1,
+      },
+    );
+  }, []);
 
   return (
     <main>
-      <h1 id="text" className="opacity-0 translate-y-10">
+      <h1 id="text" className="opacity-0 -translate-x-20">
         GsapText
       </h1>
 
